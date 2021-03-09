80007b1426db4acd172374d8ac13565652574efe
Grace Hu <gracek.hu@mail.utoronto.ca>
2021-03-08 20:21:20 -0800

    Setup database and deployment

--- deliverable-2/backend/db/mongoose.js
+++ deliverable-2/backend/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 // TODO: setup mongoose connection
-// const mongoDB = ''
-// mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/RefuTalent_API';
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 module.exports = {
   mongoose
