23fff61a9df17585ebfcd7b911a12ffc93de2dab
Grace Hu <gracek.hu@mail.utoronto.ca>
2021-04-12 21:26:31 -0700

    task9+10: Implement PUT and DELETE routes for opportunities

--- code/backend/routes/opportunity.routes.js
+++ code/backend/routes/opportunity.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { ObjectID } = require("mongodb");
-const { Opportunity } = require("../models/opportunity");
+const { Opportunity, getModel } = require("../models/opportunity");
 const { User } = require("../models/user");
 const { constructScheduleQuery, processId } = require("./helpers");
 
@@ -27,7 +27,7 @@ router.get("/api/opportunities/:id", (req, res) => {
 });
 
 // get all opportunities
-// `schedule` query should look like: mon:11-14,15-18;tues:10-14
+// `schedule` query should look like: mon:11.00-14.30,15.00-18.00;tues:10.20-14.45
 // `subjects` query should look like: MATH,BIOLOGY,HISTORY
 router.get("/api/opportunities", (req, res) => {
   const { schedule, subjects } = req.query;
@@ -64,24 +64,26 @@ router.get("/api/opportunities", (req, res) => {
 
 
 router.post("/api/opportunities", (req, res) => {
+  // commented out for now...
   // create dailySchedule objects for each weekday based on input, storing hour of interval
-  const dailySchedules = req.body.schedule.map(day => {
-    if (day.includes('')){
-      return({ available: false, hours: [] })
-    } else {
-      return({ available: true, hours: [{ start: parseInt(day[0]), end: parseInt(day[1]) }] })
-    }
-  })
-  // create and populate a schedule (TODO: task#27) - open to feedback for how schedule should be passed/initialized
-  const schedule = {
-    mon: dailySchedules[0],
-    tues: dailySchedules[1],
-    wed: dailySchedules[2],
-    thurs: dailySchedules[3],
-    fri: dailySchedules[4],
-    sat: dailySchedules[5],
-    sun: dailySchedules[6],
-  };
+  // const dailySchedules = req.body.schedule.map(day => {
+  //   if (day.includes('')){
+  //     return({ available: false, hours: [] })
+  //   } else {
+  //     return({ available: true, hours: [{ start: parseInt(day[0]), end: parseInt(day[1]) }] })
+  //   }
+  // })
+  // // create and populate a schedule (TODO: task#27) - open to feedback for how schedule should be passed/initialized
+  // const schedule = {
+  //   mon: dailySchedules[0],
+  //   tues: dailySchedules[1],
+  //   wed: dailySchedules[2],
+  //   thurs: dailySchedules[3],
+  //   fri: dailySchedules[4],
+  //   sat: dailySchedules[5],
+  //   sun: dailySchedules[6],
+  // };
+
   const userEmail = req.session.user;
   // find the user that the given user email identifies
   User.findOne({ email: userEmail }).then(
@@ -93,7 +95,7 @@ router.post("/api/opportunities", (req, res) => {
       const newOpportunity = new Opportunity({
         ...req.body,
         poster: foundUser.id,
-        schedule: schedule,
+        schedule: null,
         status: "IN REVIEW" // new opportunities should be "IN REVIEW"
       });
       newOpportunity.save().then(
@@ -110,6 +112,75 @@ router.post("/api/opportunities", (req, res) => {
       res.status(400).send({ error });
     }
   );
+});
+
+router.put("/api/opportunities/:id", async (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();
+  }
+  const id = new ObjectID(req.params.id);
+
+  const userEmail = req.session.user;
+  try {
+    const foundUser = User.findOne({ email: userEmail })
+    if (!foundUser) {
+      return res.status(404).send();
+    }
+
+    const opportunity = await Opportunity.findById(id);
+    if (!opportunity) {
+      return res.status(404).send();
+    }
+    // update opportunity only if it is created by current user
+    // or it is an admin user
+    if (foundUser.userType == "HOST" && opportunity.poster != foundUser._id) {
+      return res.status(403).send();
+    }
+    const Model = getModel(req.body.workType || opportunity.workType);
+    Model.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true, runValidators: true },
+      (err, result) => {
+        if (err || !result) {
+          res.status(400).send({ error: err || "Failed to update opportunity." });
+        } else {
+          res.send({ response: result });
+        }
+      }
+    );
+
+
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
+});
+
+router.delete("/api/opportunities/:id", async (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();
+  }
+  const id = new ObjectID(req.params.id);
+
+  const userEmail = req.session.user;
+  try {
+    const foundUser = User.findOne({ email: userEmail })
+    if (!foundUser) {
+      return res.status(404).send();
+    }
+
+    const opportunity = await Opportunity.findById(id);
+    if (!opportunity) {
+      return res.status(404).send();
+    }
+    // delete opportunity only if it is created by current user
+    // or it is an admin user
+    if (foundUser.userType == "HOST" && opportunity.poster != foundUser._id) {
+      return res.status(403).send();
+    }
+    await opportunity.remove();
+    return res.send({ response: opportunity });
+
+  } catch (error) {
+    return res.status(400).send({ error });
+  }
 });
 
 // export the router
