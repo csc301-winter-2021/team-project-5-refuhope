cd2958fff570eb922880ec9267ea49774858ce1d
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2021-04-13 22:04:54 -0400

    Updated the Post component; it is now stateless, and no longer responbile for handling edits. Instead, the logic of editing and updating the details of Posts will be moved to HostDash.

--- code/frontend/src/components/Post/Post.js
+++ code/frontend/src/components/Post/Post.js
@@ -2,160 +2,73 @@ import React, { useState } from 'react'
 import './Post.css'
 
 /**
- * Creates a mapping between the attributes of a Refugee, and labels. Enables us to programatically
- * generate a list of HTML elements. 
+ * Creates a mapping between the attributes of an Opportunity, and labels. Enables us to 
+ * programatically generate a list of HTML elements. 
  */
-
-export const POST_TEMPLATE = {
+const POST_TEMPLATE = {
     title: "TITLE",
     status: "STATUS",
-    host: "HOST",
-    workType: "WORK TYPE",
     location: "LOCATION",
-    schedule: "SCHEDULE",
+    workType: "WORK TYPE",
     hours: "HOURS PER WEEK",
     additionalInfo: "ADDITIONAL INFO"
 }
 
 /**
- * Expects props to have the following structure.
+ *  Component that represents and displays a summarized set of details of a given Opportunity.
  * 
- * {
- *  title: String,
- *  status: String,
- *  host: String,
- *  workType: String,
- *  location: String,
- *  schedule: String,
- *  hours: int,
- *  additionalInfo: String
- * }
- * 
- * In the near future, schedule should be updated so that its a 3-dimensional array.
+ *  @param {Object} props   Expects props to have the following string attributes: title, status,
+ *                          location, workType, numWorkHours, and additionalInfo. Additionally,
+ *                          props must contain an edit attribute, which is a callback that will be
+ *                          executed when the edit button is pressed.
  */
-
 const Post = (props) => {
 
-    // Stores a Posts' details
-    const [info, setInfo] = useState(props)
-
-    // Acts like a buffer for updated details
-    const [editDetailsBuffer, setEditDetailsBuffer] = useState(props)
-    const [beingEdited, setBeingEdited] = useState(false)
-
-    const handleEdit = (event, detailToUpdate) => {
-        // Modified attributes are first stored in the buffer.
-        let updatedInfo = { ...editDetailsBuffer }
-        updatedInfo[detailToUpdate] = event.target.value
-        setEditDetailsBuffer(updatedInfo)
-    }
-
-    const saveEdit = (save) => {
-        if (save) {
-            // TODO: Create a PUT request; only update state if response is OK
-            setInfo(editDetailsBuffer)
-        } else {
-            // Reset the update buffer object.
-            setEditDetailsBuffer(info)
-        }
-        setBeingEdited(false)
-    }
-
-    const createButtons = () => {
-
-        // Create the appropriate edit and save and cancel buttons.
-        if (beingEdited) {
-            return (
-                <div className="post-card-btn-tray">
-                    <button onClick={() => saveEdit(true)}>Save</button>
-                    <button onClick={() => saveEdit(false)}>Cancel</button>
-                </div>
-            )
-        } else {
-            return (
-                <div className="post-card-btn-tray">
-                    <button onClick={() => setBeingEdited(true)}>Edit</button>
-                </div>
-            )
-        }
-    }
-
     const createBody = () => {
-
-        // Create a label and Detail component for each attribute of Refugee.
+        // Create a label and Detail component for each attribute of Opportunity.
         const details = Object.keys(POST_TEMPLATE).map(key => {
             return (
-                <Detail
+                <OpportunityDetail
                     key={key}
-                    detailKey={key}
-                    beingEdited={beingEdited}
                     label={POST_TEMPLATE[key]}
-                    value={info[key]}
-                    editBufferValue={editDetailsBuffer[key]}
-                    handleEdit={handleEdit}>
-                </Detail>
+                    value={props[key]}
+                ></OpportunityDetail>
             )
         })
-
         return details
     }
 
     return (
         <div className="post-card">
-            {createButtons()}
+            <div className="post-card-btn-tray">
+                <button onClick={() => props.edit(props._id)}>Edit</button>
+            </div>
             {createBody()}
         </div>
     )
 }
 
-export const Detail = (props) => {
+export const OpportunityDetail = (props) => {
 
-    const { detailKey, beingEdited, label, value, editBufferValue, handleEdit } = props
+    const { label, value } = props
 
-    if (beingEdited) {
-        switch (label) {
-            case "ADDITIONAL INFO":
-                return (
-                    <React.Fragment>
-                        <p className="post-label">{label}</p>
-                        <textarea
-                            className="post-detail-text"
-                            value={editBufferValue}
-                            onChange={(e) => handleEdit(e, detailKey)}>
-                        </textarea>
-                    </React.Fragment>
-                )
-            default:
-                return (
-                    <React.Fragment>
-                        <p className="post-label">{label}</p>
-                        <input
-                            className="post-detail"
-                            value={editBufferValue}
-                            onChange={(e) => handleEdit(e, detailKey)}>
-                        </input>
-                    </React.Fragment>
-                )
-        }
-    } else {
-        switch (label) {
-            case "TITLE":
-                return <p className="post-title">{value}</p>
-            case "STATUS":
-                if (value === "Matched") {
-                    return <p className="post-status-matched">{value}</p>
-                }
-                else {
-                    return <p className="post-status-review">{value}</p>
-                }
-            default:
-                return (
-                    <React.Fragment>
-                        <p className="post-label">{label}</p>
-                        <p className="post-detail">{value}</p>
-                    </React.Fragment>
-                )
-        }
+    switch (label) {
+        case "TITLE":
+            return <p className="post-title">{value}</p>
+        case "STATUS":
+            if (value === "Matched") {
+                return <p className="post-status-matched">{value}</p>
+            }
+            else {
+                return <p className="post-status-review">{value}</p>
+            }
+        default:
+            return (
+                <React.Fragment>
+                    <p className="post-label">{label}</p>
+                    <p className="post-detail">{value}</p>
+                </React.Fragment>
+            )
     }
 }
 
